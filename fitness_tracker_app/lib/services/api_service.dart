83711e9502b6bf76b068c38f7351f33de495a2bd
lfoyer <laurent.foyer@insa-rouen.fr>
2025-01-04 19:53:10 +0100

    Update README.md

--- fitness_tracker_app/lib/services/api_service.dart
+++ fitness_tracker_app/lib/services/api_service.dart
@@ -1,3 +1,4 @@
+// lib/services/api_service.dart
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/activity.dart';
@@ -5,9 +6,18 @@ import '../models/goal.dart';
 
 class ApiService {
   final String baseUrl = 'http://10.0.2.2:8000/api';  // For Android emulator
-  
+  final String token = '24bc9d1709361fe6777d3512ec1dc33969bfb51d';
+
+  Map<String, String> get headers => {
+    'Content-Type': 'application/json',
+    'Authorization': 'Token $token',
+  };
+
   Future<List<Activity>> getActivities() async {
-    final response = await http.get(Uri.parse('$baseUrl/activities/'));
+    final response = await http.get(
+      Uri.parse('$baseUrl/activities/'),
+      headers: headers,
+    );
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data.map((json) => Activity.fromJson(json)).toList();
@@ -18,7 +28,7 @@ class ApiService {
   Future<Activity> createActivity(Activity activity) async {
     final response = await http.post(
       Uri.parse('$baseUrl/activities/'),
-      headers: {'Content-Type': 'application/json'},
+      headers: headers,
       body: json.encode(activity.toJson()),
     );
     if (response.statusCode == 201) {
@@ -28,11 +38,26 @@ class ApiService {
   }
 
   Future<List<Goal>> getGoals() async {
-    final response = await http.get(Uri.parse('$baseUrl/goals/'));
+    final response = await http.get(
+      Uri.parse('$baseUrl/goals/'),
+      headers: headers,
+    );
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data.map((json) => Goal.fromJson(json)).toList();
     }
     throw Exception('Failed to load goals');
+  }
+
+  Future<Goal> createGoal(Goal goal) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/goals/'),
+      headers: headers,
+      body: json.encode(goal.toJson()),
+    );
+    if (response.statusCode == 201) {
+      return Goal.fromJson(json.decode(response.body));
+    }
+    throw Exception('Failed to create goal');
   }
 }
